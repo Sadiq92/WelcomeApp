e979249bf67abf0e47cac50f98c2cfc4c3d91f6e
Sadiq <aqeelhashmi92@gmail.com>
2018-11-03 19:44:24 +0530

    added Welcome.java

---
+++ Welcome/src/main/java/com/app/Welcome.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to git hub");
+
+	}
+
+}
